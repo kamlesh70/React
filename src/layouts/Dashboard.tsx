1b9f783daec9849c837978c93605c36d2ea09b68
kamlesh70 <kamalmehra3571@gmail.com>
2024-05-02 16:12:45 +0530

    add router and dashboard

---
+++ src/layouts/Dashboard.tsx
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import { Layout, Menu, theme } from 'antd';
+import { Outlet, useLocation } from 'react-router-dom';
+import items from './nav-items';
+
+const { Header, Content, Sider } = Layout;
+
+const Dashboard: React.FC = () => {
+    const location = useLocation();
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+
+  return (
+    <Layout style={{ minHeight: '100vh' }}>
+      <Sider collapsible collapsed={collapsed} onCollapse={(value) => setCollapsed(value)}>
+        <div className="demo-logo-vertical" />
+        <Menu 
+            theme="dark" 
+            defaultSelectedKeys={ [location.pathname ?? '/'] } 
+            mode="inline" 
+            items={items} 
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }} />
+        <Content style={{ margin: '0 16px' }}>
+          <div
+            style={{
+              padding: 24,
+              minHeight: 360,
+              background: colorBgContainer,
+              borderRadius: borderRadiusLG,
+              margin: '16px 0'
+            }}
+          >
+            <Outlet />
+          </div>
+        </Content>
+      </Layout>
+    </Layout>
+  );
+};
+
+export default Dashboard;
---
+++ src/layouts/nav-items.tsx
@@ -0,0 +1,41 @@
+import {
+  DesktopOutlined,
+  FileOutlined,
+  PieChartOutlined,
+  TeamOutlined,
+  UserOutlined,
+} from '@ant-design/icons';
+import type { MenuProps } from 'antd';
+import { NavLink } from 'react-router-dom';
+
+
+type MenuItem = Required<MenuProps>['items'][number];
+
+function getItem(
+  label: React.ReactNode,
+  key: React.Key,
+  icon?: React.ReactNode,
+  children?: MenuItem[],
+): MenuItem {
+  return {
+    key,
+    icon,
+    children,
+    label,
+  } as MenuItem;
+}
+
+const items: MenuItem[] = [
+  getItem( <NavLink to='/'>Option 1</NavLink>, '/', <PieChartOutlined />),
+  getItem(<NavLink to='/router-dom'>React Router</NavLink>, '/router-dom', <DesktopOutlined />),
+  getItem('User', 'sub1', <UserOutlined />, [
+    getItem('Tom', '3'),
+    getItem('Bill', '4'),
+    getItem('Alex', '5'),
+  ]),
+  getItem('Team', 'sub2', <TeamOutlined />, [getItem('Team 1', '6'), getItem('Team 2', '8')]),
+  getItem('Files', '9', <FileOutlined />),
+];
+
+
+export default items;
