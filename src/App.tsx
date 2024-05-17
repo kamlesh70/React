e94b7cc30a0c989f4e281998109408fdf2a4b9a0
kamlesh70 <kamalmehra3571@gmail.com>
2024-05-17 17:22:34 +0530

    add redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import { RouterProvider } from "react-router-dom"
 import router from "./router/router"
 import "./App.css";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
 
   return (
     <>
+    <Provider store={store}>
       <RouterProvider router={router} />
+    </Provider>
     </>
   )
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { login } from "../../store/slices/userSlice";
+
 function Home() {
+  const user = useSelector((state: any) => state.userList);
+  const dispatch = useDispatch();
+  console.log(user);
+  useEffect(() => {
+    dispatch(login());
+  }, [])
   return (
     <div>Home</div>
   )
